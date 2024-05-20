665174ad672d7d907bc9c299170f0d949301f84d
Pedro Araujo <araujopedrop@gmail.com>
2024-05-20 04:07:05 +0000

    robot_chase node gets transform between rick and morty base_links and sends commands to searcher barista robot

--- src/robot_chase.cpp
+++ src/robot_chase.cpp
@@ -12,20 +12,6 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/buffer.h>
 
-class Coordinates {
-public:
-  Coordinates(float x, float y, float z, float roll, float pitch, float yaw)
-      : x(x), y(y), z(z), roll(roll), pitch(pitch), yaw(yaw) {}
-
-private:
-  float x;
-  float y;
-  float z;
-  float roll;
-  float pitch;
-  float yaw;
-};
-
 class RobotChase : public rclcpp::Node {
 public:
   explicit RobotChase(
@@ -34,8 +20,6 @@ public:
 
     using namespace std::placeholders;
 
-    ptr_objCoordinates =
-        std::make_shared<Coordinates>(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
@@ -44,7 +28,8 @@ public:
 
     // control loop of 100 Hz
     this->timer_check_bot_pos_ = this->create_wall_timer(
-        std::chrono::milliseconds(100), std::bind(&RobotChase::move_bot, this));
+        std::chrono::milliseconds(100),
+        std::bind(&RobotChase::move_bot_to_coordinates, this));
 
     this->publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
         this->robot_catcher + "/cmd_vel", 10);
@@ -52,28 +37,21 @@ public:
     this->sub_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
         this->robot_catcher + "/odom", 10,
         std::bind(&RobotChase::odom_callback, this, _1));
+
+    RCLCPP_INFO(this->get_logger(), "Robot_chase_node is up!");
   }
 
 private:
-  /*
-
-
-
-  geometry_msgs::msg::Pose2D desired_pos_;
-  
   geometry_msgs::msg::Twist robot_vel_;
-
-  */
-  
   geometry_msgs::msg::Pose2D current_pos_;
 
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   geometry_msgs::msg::TransformStamped desired_pos_;
   rclcpp::TimerBase::SharedPtr timer_check_bot_pos_;
+
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<Coordinates> ptr_objCoordinates;
 
   const std::string robot_catcher = "rick";
   const std::string robot_escaper = "morty";
@@ -81,20 +59,10 @@ private:
   std::string parent_frame_;
   std::string child_frame_;
 
-  void move_bot() {
-
-    if (ptr_objCoordinates) {
-      this->move_bot_to_coordinates();
-    } else {
-    }
-  }
-
-  void move_bot_to_coordinates() {
+  void get_desired_pos() {
 
     this->desired_pos_ =
         this->get_model_pose_from_tf(this->parent_frame_, this->child_frame_);
-
-    this->desired_pos_.transform.translation.x;
   }
 
   // Get transformation between parent_frame and child_frame
@@ -128,30 +96,35 @@ private:
     return ts_;
   }
 
-  /*
-
-  void execute() {
+  void move_bot_to_coordinates() {
 
     float threshold_ = 0.1;
     float current_angle_ = 0.0;
-    float desi
-    float current_y_ = 0.0;
     float desired_x_ = 0.0;
     float desired_y_ = 0.0;
+    float desired_angle_ = 0.0;
 
-    RCLCPP_INFO(this->get_logger(), "Executing goal");
-red_angle_ = 0.0;
     float target_angle_ = 0.0;
     float error_angle_ = 0.0;
     float current_x_ = 0.0;
+    float current_y_ = 0.0;
+
+    RCLCPP_INFO(this->get_logger(), "Executing goal");
+
+    // Control loop
+    rclcpp::Rate loop_rate(10);
+
     // get data for control
     current_x_ = this->current_pos_.x;
     current_y_ = this->current_pos_.y;
-    desired_x_ = this->desired_pos_.x;
-    desired_y_ = this->desired_pos_.y;
+    desired_x_ = this->desired_pos_.transform.translation.x;
+    desired_y_ = this->desired_pos_.transform.translation.y;
     current_angle_ = this->current_pos_.theta;
-    desired_angle_ = this->desired_pos_.theta *
-                     0.01745; // Action input is given in degrees: *(2*PI / 360)
+    desired_angle_ = 0.0;
+
+    // desired_angle_ = this->desired_pos_.theta *
+    //                  0.01745; // Action input is given in degrees: *(2*PI /
+    //                  360)
 
     // Show in terminal
     RCLCPP_INFO(this->get_logger(), "Current position: x: %f - y: %f",
@@ -164,23 +137,9 @@ red_angle_ = 0.0;
                 std::abs(current_x_ - desired_x_),
                 std::abs(current_y_ - desired_y_));
 
-    // Control loop
-    rclcpp::Rate loop_rate(10);
     while (this->goal_reached(current_x_, current_y_, current_angle_,
                               desired_x_, desired_y_, desired_angle_,
                               threshold_) == false) {
-
-      // Check if goal is cancelled
-      if (true) {
-
-        // Stop robot
-        this->robot_vel_.linear.x = 0.0;
-        this->robot_vel_.angular.z = 0.0;
-        publisher_->publish(this->robot_vel_);
-
-        RCLCPP_INFO(this->get_logger(), "Goal canceled");
-        return;
-      }
 
       // If I reached position, start searching desired orientation
       // If not, continue searching position
@@ -216,6 +175,17 @@ red_angle_ = 0.0;
       current_y_ = this->current_pos_.y;
       current_angle_ = this->current_pos_.theta;
 
+      // Show in terminal
+      RCLCPP_INFO(this->get_logger(), "Current position: x: %f - y: %f",
+                  current_x_, current_y_);
+
+      RCLCPP_INFO(this->get_logger(), "Desired position: x: %f - y: %f",
+                  desired_x_, desired_y_);
+
+      RCLCPP_INFO(this->get_logger(), "Delta x: %f - Delta y: %f",
+                  std::abs(current_x_ - desired_x_),
+                  std::abs(current_y_ - desired_y_));
+
       loop_rate.sleep();
     }
 
@@ -245,8 +215,6 @@ red_angle_ = 0.0;
     return false;
   }
 
-
-
   bool goal_reached(float current_x, float current_y, float current_angle,
                     float desired_x, float desired_y, float desired_angle,
                     float threshold) {
@@ -262,7 +230,6 @@ red_angle_ = 0.0;
     return false;
   }
 
-*/
   void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
 
     geometry_msgs::msg::Quaternion quaternion_;
@@ -274,27 +241,22 @@ red_angle_ = 0.0;
 
     x_ = msg->pose.pose.position.x;
     y_ = msg->pose.pose.position.y;
-
+    yaw = 0.0;
     // Quaterion 2 Euler
-
 
     quaternion_ = msg->pose.pose.orientation;
 
-    //tf2::Quaternion tf_quaternion;
-    //tf2::fromMsg(quaternion_, tf_quaternion);
-    //tf2::Matrix3x3(tf_quaternion).getRPY(roll, pitch, yaw);
-
+    tf2::Quaternion tf_quaternion;
+    // tf2::fromMsg(quaternion_, tf_quaternion);
+    // tf2::fromMsg(quaternion_, tf_quaternion);
+    // tf2::Matrix3x3(tf_quaternion).getRPY(roll, pitch, yaw);
 
     this->current_pos_.x = x_;
     this->current_pos_.y = y_;
     this->current_pos_.theta = yaw;
 
-
+    this->get_desired_pos();
   }
-
-  
-
-
 
 }; // class GoToPose
 
